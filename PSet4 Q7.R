2ade348ba3af4f6812093f0e0ca7cebd1d022871
weastwick <willeastwick@yahoo.com>
2021-05-12 21:36:22 -0500

    Update with part a

--- PSet4 Q7.R
+++ PSet4 Q7.R
@@ -1,3 +1,23 @@
 library(readxl)
 
-data <- read_excel("birthweight_smoking.xlsx")
+data <- read_excel("~/Documents/GitHub/Econ-210/birthweight_smoking.xlsx")
+
+#part a
+
+lm(birthweight ~ smoker, data = data)
+
+#part a
+    # Estimated difference in average birthweight for smokers vs.
+    # non-smokers is 253.2 grams
+
+#part b
+
+model <- lm(birthweight ~ smoker + alcohol + nprevist, data = data)
+
+#part b, i
+    # If we wish to interpret part (a)'s regression as causal, other variables
+    # such as Alcohol and Nprevist that may have a causal effect will be
+    # excluded, which constitutes as omitted variable bias.
+
+#part b, ii
+    # 
